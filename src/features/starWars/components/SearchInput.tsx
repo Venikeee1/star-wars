7fd629746f224a8b27095b3c08832d8996940f56
oleksandr_vienik <alexandrVenik@gmail.com>
2023-10-19 03:25:28 +0300

    SearchInput ui improvements

--- src/features/starWars/components/SearchInput.tsx
+++ src/features/starWars/components/SearchInput.tsx
@@ -10,7 +10,7 @@ export const SearchInput = (props: SearchInputProps) => {
 
 	return (
 		<div className="relative mb-2">
-			<div className="relative w-80 transition-[width] duration-300 focus-within:w-full">
+			<div className="relative w-full transition-[width] duration-300 focus-within:w-full md:w-80">
 				<Icon
 					name="yoda"
 					className="absolute bottom-0 right-2 top-0 my-auto h-[80%] w-auto"
@@ -19,7 +19,7 @@ export const SearchInput = (props: SearchInputProps) => {
 					type="text"
 					{...props}
 					placeholder="Search your force"
-					className="font text-md w-full border-spacing-2 rounded-md py-3 pl-2 pr-10"
+					className="font text-md w- w-full border-spacing-2 rounded-md bg-[#3b3b3b] py-3 pl-2 pr-10 focus:outline-cyan-500"
 					onFocus={() => {
 						play()
 					}}
