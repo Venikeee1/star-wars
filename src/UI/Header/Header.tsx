8835cb55c1e132fa7635550a1bbf25a9be4a5210
oleksandr_vienik <alexandrVenik@gmail.com>
2023-10-19 03:25:10 +0300

    Header ui improvements

--- src/UI/Header/Header.tsx
+++ src/UI/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Icon } from '../Icons/Icons'
 
 export const Header = () => {
 	return (
-		<header className="bg-primary sticky top-0 flex justify-center p-4">
+		<header className="sticky top-0 z-50 flex justify-center bg-primary p-4">
 			<Icon name="logo" className="h-auto w-[100px]" />
 		</header>
 	)
